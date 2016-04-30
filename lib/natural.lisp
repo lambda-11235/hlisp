b776f4f30d60772c5675f8f8e517ad3c02c5dc2e
Taran Lynn <taranlynn0@gmail.com>
2016-04-30 16:17:23 -0700

    Added a function to convert digits to a natural number.

--- lib/natural.lisp
+++ lib/natural.lisp
@@ -23,6 +23,7 @@
 (label 7 (succ 6))
 (label 8 (succ 7))
 (label 9 (succ 8))
+(label 10 (succ 9))
 
 
 (defun + (x y) (cond (= x 0) y
@@ -50,3 +51,10 @@
 (defun fact (x) (if (= x 0)
                     1
                   (* x (fact (pred x)))))
+
+
+;; (fromDigs 1 2 0) = 120
+(defun fromDigs-0 (n ds) (cond (nil? ds) n
+                               't (fromDigs-0 (+ (* 10 n) (car ds)) (cdr ds))))
+
+(defun fromDigs ds (fromDigs-0 0 ds))
