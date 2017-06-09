a999692ac8c44531b3cec85e3361823b677936fa
Taran Lynn <taranlynn0@gmail.com>
2017-06-08 22:59:15 -0700

    Added some list functions.

--- lib/core.lisp
+++ lib/core.lisp
@@ -3,13 +3,6 @@
 
 (label list (lambda xs xs))
 
-(label nil? (lambda (x) (= x ())))
-
-(label caar (lambda (xs) (car (car xs))))
-(label cadr (lambda (xs) (car (cdr xs))))
-(label cdar (lambda (xs) (cdr (car xs))))
-(label cddr (lambda (xs) (cdr (cdr xs))))
-
 
 (label true (lambda (x y) x))
 (label false (lambda (x y) y))
@@ -19,20 +12,32 @@
 (label or (lambda (x y) (x x y)))
 
 
-(label id (lambda (x) x))
-(label const (lambda (x) (lambda (y) x)))
+(label nil? (lambda (x) (= x ())))
+(label single? (lambda (xs) (and (not (nil? xs)) (nil? (cdr xs)))))
 
+(label caar (lambda (xs) (car (car xs))))
+(label cadr (lambda (xs) (car (cdr xs))))
+(label cdar (lambda (xs) (cdr (car xs))))
+(label cddr (lambda (xs) (cdr (cdr xs))))
 
 (label map (lambda map (f xs)
              ((nil? xs)
               ()
               (cons (f (car xs)) (map f (cdr xs))))))
 
-
 (label reduce (lambda reduce (f x xs)
                 ((nil? xs)
                  x
                  (reduce f (f x (car xs)) (cdr xs)))))
 
+(label filter (lambda filter (f xs)
+                ((nil? xs)
+                 ()
+                 ((f (car xs)) (cons (car xs) (filter f (cdr xs)))
+                               (filter f (cdr xs))))))
 
 (label last (lambda (xs) (reduce (lambda (x y) y) () xs)))
+
+
+(label id (lambda (x) x))
+(label const (lambda (x) (lambda (y) x)))
